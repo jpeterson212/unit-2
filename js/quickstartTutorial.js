5e3ad2c2f884878603c11f3e3445ca3456ec8e62
jpeterson212 <60198477+jpeterson212@users.noreply.github.com>
2020-02-18 22:45:48 -0600

    commented out tut and quick

--- js/quickstartTutorial.js
+++ js/quickstartTutorial.js
@@ -1,3 +1,4 @@
+//Setting map parameters (the view)
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -7,8 +8,10 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1IjoicGV0ZXJzb24yIiwiYSI6ImNrNmpza3ZwNDAweXEzZXF0bGxmb2g5eTQifQ.10E8d50dRzp7rkDlUiEj_g'
 }).addTo(mymap);
 
+//Creates a marker on the map.
 var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
+//Creates a red circle
 var circle = L.circle([51.508, -0.11], {
     color: 'red',
     fillColor: '#f03',
@@ -16,16 +19,24 @@ var circle = L.circle([51.508, -0.11], {
     radius: 500
 }).addTo(mymap);
 
+//Creates a polygon.
 var polygon = L.polygon([
     [51.509, -0.08],
     [51.503, -0.06],
     [51.51, -0.047]
 ]).addTo(mymap);
 
+//Message will appear on clicking the marker.
 marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+//Message will appear when you click on the circle.
 circle.bindPopup("I am a circle.");
+
+//Message will appear when click on the polygon.
 polygon.bindPopup("I am a polygon.");
 
+//This appears right when you open the page. The popup says
+// 'I am a standaone popup'
 var popup = L.popup()
     .setLatLng([51.5, -0.09])
     .setContent("I am a standalone popup.")
@@ -39,6 +50,8 @@ mymap.on('click', onMapClick);
 
 var popup = L.popup();
 
+//This makes a popup message appear with the lat and lon of the
+// location you clicked.
 function onMapClick(e) {
     popup
         .setLatLng(e.latlng)
